872c520bc26e0b2d8f09988dc1bd95c67e1a62ab
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 22:15:49 +0100

    Initial import

---
+++ GeosansLight.h
@@ -0,0 +1,39 @@
+#ifndef _GEOSANSLIGHT_H
+#define _GEOSANSLIGHT_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t GeosansLight10[];
+    extern const uint8_t GeosansLight11[];
+    extern const uint8_t GeosansLight12[];
+    extern const uint8_t GeosansLight13[];
+    extern const uint8_t GeosansLight14[];
+    extern const uint8_t GeosansLight15[];
+    extern const uint8_t GeosansLight16[];
+    extern const uint8_t GeosansLight17[];
+    extern const uint8_t GeosansLight18[];
+    extern const uint8_t GeosansLight19[];
+    extern const uint8_t GeosansLight20[];
+    extern const uint8_t GeosansLight21[];
+    extern const uint8_t GeosansLight22[];
+    extern const uint8_t GeosansLight23[];
+    extern const uint8_t GeosansLight24[];
+    extern const uint8_t GeosansLight25[];
+    extern const uint8_t GeosansLight26[];
+    extern const uint8_t GeosansLight27[];
+    extern const uint8_t GeosansLight28[];
+    extern const uint8_t GeosansLight29[];
+    extern const uint8_t GeosansLight30[];
+    extern const uint8_t GeosansLight31[];
+    extern const uint8_t GeosansLight32[];
+    extern const uint8_t GeosansLight33[];
+    extern const uint8_t GeosansLight34[];
+    extern const uint8_t GeosansLight35[];
+    extern const uint8_t GeosansLight36[];
+    extern const uint8_t GeosansLight37[];
+    extern const uint8_t GeosansLight38[];
+    extern const uint8_t GeosansLight39[];
+    extern const uint8_t GeosansLight40[];
+};
+#endif
